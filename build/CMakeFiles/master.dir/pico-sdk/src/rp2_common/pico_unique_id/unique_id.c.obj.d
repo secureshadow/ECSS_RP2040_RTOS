7b6de95477bc469cd69a41961655fd8f2c1ba37a
vladik <evcegarra@protonmail.com>
2023-12-19 14:06:54 +0100

    Establish I2C communication between two raspberry pi pico

---
+++ build/CMakeFiles/master.dir/pico-sdk/src/rp2_common/pico_unique_id/unique_id.c.obj.d
@@ -0,0 +1,27 @@
+CMakeFiles/master.dir/pico-sdk/src/rp2_common/pico_unique_id/unique_id.c.obj: \
+ /home/vladik/Documentos/ECSS_RP2040_RTOS/pico-sdk/src/rp2_common/pico_unique_id/unique_id.c \
+ /home/vladik/Documentos/ECSS_RP2040_RTOS/pico-sdk/src/rp2_common/hardware_flash/include/hardware/flash.h \
+ /home/vladik/Documentos/ECSS_RP2040_RTOS/pico-sdk/src/common/pico_base/include/pico.h \
+ /home/vladik/Documentos/ECSS_RP2040_RTOS/pico-sdk/src/common/pico_base/include/pico/types.h \
+ /home/vladik/Documentos/ECSS_RP2040_RTOS/pico-sdk/src/common/pico_base/include/pico/assert.h \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stdbool.h \
+ /usr/include/newlib/assert.h /usr/include/newlib/_ansi.h \
+ /usr/include/newlib/newlib.h /usr/include/newlib/_newlib_version.h \
+ /usr/include/newlib/sys/config.h /usr/include/newlib/machine/ieeefp.h \
+ /usr/include/newlib/sys/features.h \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stddef.h \
+ /home/vladik/Documentos/ECSS_RP2040_RTOS/build/generated/pico_base/pico/version.h \
+ /home/vladik/Documentos/ECSS_RP2040_RTOS/pico-sdk/src/common/pico_base/include/pico/config.h \
+ /home/vladik/Documentos/ECSS_RP2040_RTOS/build/generated/pico_base/pico/config_autogen.h \
+ /home/vladik/Documentos/ECSS_RP2040_RTOS/pico-sdk/src/boards/include/boards/pico.h \
+ /home/vladik/Documentos/ECSS_RP2040_RTOS/pico-sdk/src/rp2_common/cmsis/include/cmsis/rename_exceptions.h \
+ /home/vladik/Documentos/ECSS_RP2040_RTOS/pico-sdk/src/rp2_common/pico_platform/include/pico/platform.h \
+ /home/vladik/Documentos/ECSS_RP2040_RTOS/pico-sdk/src/rp2040/hardware_regs/include/hardware/platform_defs.h \
+ /home/vladik/Documentos/ECSS_RP2040_RTOS/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/addressmap.h \
+ /home/vladik/Documentos/ECSS_RP2040_RTOS/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/sio.h \
+ /usr/include/newlib/sys/cdefs.h \
+ /usr/include/newlib/machine/_default_types.h \
+ /home/vladik/Documentos/ECSS_RP2040_RTOS/pico-sdk/src/common/pico_base/include/pico/types.h \
+ /home/vladik/Documentos/ECSS_RP2040_RTOS/pico-sdk/src/common/pico_base/include/pico/error.h \
+ /home/vladik/Documentos/ECSS_RP2040_RTOS/pico-sdk/src/rp2_common/pico_unique_id/include/pico/unique_id.h
