816c0599ff780c059afd8362ee36fbbf419bce51
vladik <evcegarra@protonmail.com>
2023-12-19 18:56:57 +0100

    Print the temperature from the BMP180 sensor

--- build/CMakeFiles/Makefile.cmake
+++ build/CMakeFiles/Makefile.cmake
@@ -316,8 +316,7 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/master.dir/DependInfo.cmake"
-  "CMakeFiles/slave.dir/DependInfo.cmake"
+  "CMakeFiles/blink.dir/DependInfo.cmake"
   "CMakeFiles/FreeRTOS.dir/DependInfo.cmake"
   "CMakeFiles/ELF2UF2Build.dir/DependInfo.cmake"
   "pico-sdk/src/rp2_common/boot_stage2/CMakeFiles/bs2_default.dir/DependInfo.cmake"
